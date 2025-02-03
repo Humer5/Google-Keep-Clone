8f2bdca56e289a2607c4032a79c4d9d06d3e6326
Humer5 <humerresham@gmail.com>
2025-02-03 11:32:40 +0530

    updated all features

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { createSignal } from "solid-js";
 import Layout from "./components/Layout/layout";
 
 
-// Sample content for demonstration
 const App = () => {
   // const [notes, setNotes] = createSignal([]);
  
@@ -17,17 +16,6 @@ const App = () => {
         {/* Notes input box */}
         <div class="note-input">
           <textarea placeholder="Take a note..." />
-          {/* <div class="note-actions">
-            <button>
-              <span class="material-symbols-outlined">check_box</span>
-            </button>
-            <button>
-              <span class="material-symbols-outlined">brush</span>
-            </button>
-            <button>
-              <span class="material-symbols-outlined">image</span>
-            </button>
-          </div> */}
         </div>
 
         {/* Notes list */}
